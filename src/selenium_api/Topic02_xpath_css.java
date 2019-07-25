36e173e81355a61332ec30c8eb363102628a3172
Brian Nguyen <Brian Nguyen@DESKTOP-QJOTTVV>
2019-07-25 08:12:40 +0700

    learn xpath

--- src/selenium_api/Topic02_xpath_css.java
+++ src/selenium_api/Topic02_xpath_css.java
@@ -1,6 +1,8 @@
 package selenium_api;
 
 import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
@@ -14,21 +16,36 @@ public class Topic02_xpath_css {
 	@BeforeClass
 	public void beforeClass() {
 		driver = new FirefoxDriver();
-		driver.get("http://live.guru99.com");
+		driver.get("https://truong.membrain.com/Login.aspx");
 		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-		driver.manage().window().maximize();
+//		driver.manage().window().maximize();
 	}
 
 	@Test
-	public void TC_01() {
-		System.setProperty("webdriver.gecko.driver", "D:\\geckodriver.exe");
-		String homePageTitle = driver.getTitle();
-		Assert.assertEquals("Home page", homePageTitle);
+	public void TC_01_Login() {
+//		System.setProperty("webdriver.gecko.driver", "D:\\geckodriver.exe");
+		String pageTitle = driver.getTitle();
+		Assert.assertEquals("Membrain", pageTitle);
+		
+//		driver.findElement(By.xpath("//input[@id='eMailInput']")).click();
+		driver.findElement(By.xpath("//input[@id='eMailInput']")).sendKeys("truong.nguyen@membrain.com");
+		driver.findElement(By.xpath("//input[@id='passwordInput']")).sendKeys("naT@31390");
+		
+//		driver.findElement(By.xpath("/html/body/div/form/div[1]/div[2]/div[1]/div[2]/table/tbody/tr/td[1]/div")).click();
+//		driver.findElement(By.xpath("//div[@class='LoginButtonContainer']//div[@class='Button GreenButton']")).click();
+		driver.findElement(By.xpath("//div[@class='LoginButtonContainer']//div[contains(text(), 'Login')]")).click();
+		
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		pageTitle = driver.getTitle(); 
+		Assert.assertEquals("Membrain for Membrain for Truong", pageTitle);
+		
+		
 	}
 
 	@AfterClass
 	public void afterClass() {
-		driver.quit();
+//		driver.quit();
 	}
 
 }
