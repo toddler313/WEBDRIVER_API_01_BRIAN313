7a10d3c673a66b86bda4ae168ef2b193fa1cfff7
Brian Nguyen <Brian Nguyen@DESKTOP-QJOTTVV>
2019-07-25 08:16:37 +0700

    learn xpath

--- src/selenium_api/Topic02_xpath_css.java
+++ src/selenium_api/Topic02_xpath_css.java
@@ -26,21 +26,20 @@ public class Topic02_xpath_css {
 //		System.setProperty("webdriver.gecko.driver", "D:\\geckodriver.exe");
 		String pageTitle = driver.getTitle();
 		Assert.assertEquals("Membrain", pageTitle);
-		
+
 //		driver.findElement(By.xpath("//input[@id='eMailInput']")).click();
 		driver.findElement(By.xpath("//input[@id='eMailInput']")).sendKeys("truong.nguyen@membrain.com");
 		driver.findElement(By.xpath("//input[@id='passwordInput']")).sendKeys("naT@31390");
-		
+
 //		driver.findElement(By.xpath("/html/body/div/form/div[1]/div[2]/div[1]/div[2]/table/tbody/tr/td[1]/div")).click();
 //		driver.findElement(By.xpath("//div[@class='LoginButtonContainer']//div[@class='Button GreenButton']")).click();
 		driver.findElement(By.xpath("//div[@class='LoginButtonContainer']//div[contains(text(), 'Login')]")).click();
-		
+
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		      
-		pageTitle = driver.getTitle(); 
+
+		pageTitle = driver.getTitle();
 		Assert.assertEquals("Membrain for Membrain for Truong", pageTitle);
-		
-		
+
 	}
 
 	@AfterClass
